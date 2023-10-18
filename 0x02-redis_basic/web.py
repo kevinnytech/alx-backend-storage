9b2869066c9356bdaf52931162abf180e58d18a2
kevinnytech <berlinstation254@gmail.com>
2023-10-18 23:59:24 +0300

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+""" 5. Implementing an expiring web cache and tracker """
+
 import redis
 import requests
-from functools import wraps
-from typing import Callable
 
+def cache(expiration_time: int):
+    def decorator(func):
+        def wrapper(url):
+            key = f"count:{url}"
+            cache = redis.Redis()
+            content = cache.get(url)
+            if content is None:
+                r = requests.get(url)
+                content = r.content.decode()
+                cache.setex(url, expiration_time, content)
+            cache.incr(key)
+            return content
+        return wrapper
+    return decorator
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
+@cache(10)
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    Returns the HTML content of a particular URL.
+
+    Args:
+        url (str): The URL to retrieve.
+
+    Returns:
+        str: The HTML content of the URL.
+    """
+    r = requests.get(url)
+    content = r.content.decode()
+    key = f"count:{url}"
+    cache = redis.Redis()
+    cache.incr(key)
+    cache.expire(key, 10)
+    return content
